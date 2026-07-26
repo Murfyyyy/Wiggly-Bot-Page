ef719ed1cd27beaa46e52d536c0790c37b65c155
Murfyy <mofadopessoal876@gmail.com>
2026-07-26 04:17:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const link = document.getElementById('discord-link');
-const sound = document.getElementById('click-sound');
-
-link.addEventListener('click', () => {
-    sound.currentTime = 0;
-    sound.play();
-});
